18a458fe98b9227a3619f81210dd049d4f6d6f3d
baltazarparra <baltazar.parra.ext@dasa.com.br>
2021-06-25 08:48:53 -0300

    update boss

--- win.js
+++ win.js
@@ -1,9 +1,11 @@
 import k from './kaboom.js'
 
 export default function win () {
-    add([text('YOU WON'), origin('center'), pos(width()/2, height()/2)])
+    add([text('YOU WON', 40), origin('center'), pos(width()/2, height()/2)])
     
     mouseClick(() => {
-        go('start')
+        wait(1, () => {
+            go('start')
+        })
     })
 }
